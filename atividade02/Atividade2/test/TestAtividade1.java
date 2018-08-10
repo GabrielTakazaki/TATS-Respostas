932ffe94123e404015022e7c516415c8e6acb600
Gabriel Takazaki <gabriel_takazaki@hotmail.com>
2018-08-10 16:34:49 -0300

    Teste Completo

--- atividade02/Atividade2/test/TestAtividade1.java
+++ atividade02/Atividade2/test/TestAtividade1.java
@@ -8,25 +8,78 @@ import static org.mockito.Mockito.*;
 
 
 public class TestAtividade1 {
+    UsuarioDAO usuDAO = mock(UsuarioDAO.class);
+    SenhaValidator senVAL = mock(SenhaValidator.class);
+    
+    UsuarioValidator usuVal = new UsuarioValidator(usuDAO);
     
     public TestAtividade1() {
         
     }
+    
     @Test
-    public void Test1() throws Exception {
+    public void TestUsuarioValido() throws Exception {
         Usuario u = new Usuario();
         u.setNome("Gabriel");
-        u.setSenha("123");
-        u.setSenhaConfirmada("123");
+        u.setSenha("12345");
+        u.setSenhaConfirmada("12345");
+        usuVal.setSenhaValidator(senVAL);
         
-        UsuarioDAO usuarioMocked = mock(UsuarioDAO.class);
-        SenhaValidator senhaMocked = mock(SenhaValidator.class);
-        
-        UsuarioValidator usuVal = new UsuarioValidator(usuarioMocked);
-        
-        usuarioMocked.getByName("Gabriel");
-        senhaMocked.verificar("123");  
-        
+        when(senVAL.verificar("12345")).thenReturn(false);
         assertTrue(usuVal.ehUsuarioValido(u));
+    }
+    
+    @Test(expected = Exception.class)
+    public void TestNomeInvalido() throws Exception {
+        Usuario u = new Usuario();
+        u.setNome("ga");
+        when(usuVal.ehUsuarioValido(u)).thenThrow(Exception.class);
+    }
+    
+    @Test(expected = Exception.class)
+    public void TestSenhaInvalido() throws Exception {
+        Usuario u = new Usuario();
+        u.setNome("Gabriel");
+        u.setSenha("12345");
+        u.setSenhaConfirmada("1234");
+        when(usuVal.ehUsuarioValido(u)).thenThrow(Exception.class);        
+    }
+    
+    @Test(expected = Exception.class)
+    public void TestSenhaValidatorInvalido() throws Exception {
+        Usuario u = new Usuario();
+        u.setNome("Gabriel");
+        u.setSenha("12345");
+        u.setSenhaConfirmada("12345");
+        usuVal.setSenhaValidator(senVAL);
+        
+        when(senVAL.verificar("12345")).thenReturn(true);
+        usuVal.ehUsuarioValido(u);
+    }
+    
+    @Test
+    public void TestDigitoNome() throws Exception {
+        Usuario u = new Usuario();
+        u.setNome("G4br1el");
+        u.setSenha("12345");
+        u.setSenhaConfirmada("12345");
+        
+        usuVal.setSenhaValidator(senVAL);
+        
+        when(senVAL.verificar("12345")).thenReturn(false);
+        assertTrue(usuVal.ehUsuarioValido(u));
+    }
+    
+    @Test
+    public void TestNomeEspecial() throws Exception {
+        Usuario u = new Usuario();
+        u.setNome("!!!el");
+        u.setSenha("12345");
+        u.setSenhaConfirmada("12345");
+        
+        usuVal.setSenhaValidator(senVAL);
+        
+        when(senVAL.verificar("12345")).thenReturn(false);
+        assertFalse(usuVal.ehUsuarioValido(u));
     }
 }
