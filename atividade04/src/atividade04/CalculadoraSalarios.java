930b03075cf3742d374b441b36b95435e2c41791
GabrielTakazaki <gabriel_takazaki@hotmail.com>
2018-08-16 20:26:07 -0300

    Atividade 04 sem TDD

---
+++ atividade04/src/atividade04/CalculadoraSalarios.java
@@ -0,0 +1,39 @@
+package atividade04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculadoraSalarios {
+
+    public void calculoSalario(Funcionario f) {
+        if (f.getCargo().equals("DESENVOLVEDOR")) {
+            if (f.getSalariobase() < 3000f) {
+                f.setSalarioLiquido(f.getSalariobase() - (f.getSalariobase() * 0.10f));
+            } else {
+                f.setSalarioLiquido(f.getSalariobase() - (f.getSalariobase() * 0.20f));
+            }
+        }
+        if (f.getCargo().equals("DBA")) {
+            if (f.getSalariobase() < 2000f) {
+                f.setSalarioLiquido(f.getSalariobase() - (f.getSalariobase() * 0.15f));
+            } else {
+                f.setSalarioLiquido(f.getSalariobase() - (f.getSalariobase() * 0.25f));
+            }
+        }
+        if (f.getCargo().equals("TESTADOR")) {
+            if (f.getSalariobase() < 2000f) {
+                f.setSalarioLiquido(f.getSalariobase() - (f.getSalariobase() * 0.15f));
+            } else {
+                f.setSalarioLiquido(f.getSalariobase() - (f.getSalariobase() * 0.25f));
+            }
+        }
+        if (f.getCargo().equals("GERENTE")) {
+            if (f.getSalariobase() < 5000f) {
+                f.setSalarioLiquido(f.getSalariobase() - (f.getSalariobase() * 0.20f));
+            } else {
+                f.setSalarioLiquido(f.getSalariobase() - (f.getSalariobase() * 0.30f));
+            }
+        }
+
+    }
+}
---
+++ atividade04/src/atividade04/Funcionario.java
@@ -0,0 +1,49 @@
+package atividade04;
+
+public class Funcionario {
+    String nome;
+    String email;
+    float salariobase;
+    String cargo;
+    float salarioLiquido;
+
+    public float getSalarioLiquido() {
+        return salarioLiquido;
+    }
+
+    public void setSalarioLiquido(float salarioLiquido) {
+        this.salarioLiquido = salarioLiquido;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getSalariobase() {
+        return salariobase;
+    }
+
+    public void setSalariobase(float salariobase) {
+        this.salariobase = salariobase;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
