c69ca1e55be602dc7f5b86677313eb774fdd6617
GabrielTakazaki <gabriel_takazaki@hotmail.com>
2018-08-20 22:36:24 -0300

    Teste cargo inexistente erro

--- atividade04/test/tdd04Test.java
+++ atividade04/test/tdd04Test.java
@@ -1,3 +1,4 @@
+import atividade04.CalculadoraSalarios;
 import atividade04.Funcionario;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -7,14 +8,26 @@ public class tdd04Test {
     public tdd04Test() {
     }
     @Test
-    public void funcionarioVazio () {
+    public void funcionarioVazioTest () {
         Funcionario f = new Funcionario ();
         f.setNome("");
         f.setEmail("");
         f.setCargo("");
         f.setSalariobase(0f);
         assertEquals("nome vazio\nemail vazio\n"
-                + "cargo vazio\nsalario base vazio\n", f.getMsg);
+                + "cargo vazio\nsalario base vazio\n", f.getMsg());
     }
+    @Test
+    public void  testeCargoInexistente() {
+        Funcionario f = new Funcionario ();
+        f.setNome("G");
+        f.setEmail("G");
+        f.setCargo("A");
+        f.setSalariobase(2000f);
+        CalculadoraSalarios calc = new CalculadoraSalarios();
+        calc.calculoSalario(f);
+        assertEquals("Cargo inexistente", f.getMsgCargo());
+    }
+    
     
 }
